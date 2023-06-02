5fd66751e4951c27561679363190ca5965987fee
pleszboldi <70632121+pleszboldi@users.noreply.github.com>
2023-06-02 10:43:50 +0200

    1., 2. és 3. feladat kommentelve

--- submission.py
+++ submission.py
@@ -3,90 +3,86 @@ import numpy as np
 import math
 import scipy as sc
 
+#Napi arfolyamok beolvasasa excelbol:
 P_daily = pd.read_excel('gold_oil_.xlsx')
-
 np_P=np.array(P_daily)
 
+#Napi loghozamok szamolasa:
 r_gold=np.log(np.divide(np.subtract(np_P[1:,0], np_P[:-1,0]),np.absolute(np_P[:-1,0]))+1)
 r_oil=np.log(np.divide(np.subtract(np_P[1:,1], np_P[:-1,1]),np.absolute(np_P[:-1,1]))+1)
 
+#Portfolio hozam idosort generalo fuggveny 2 hozam idosorbol es 2 parameterbol:
 def my_portfolio(np_a,np_b,lookback,pw):
 	n=np_a.size
-	T=n-lookback
-	r_portfolio=np.empty([T])
-	for t in range(T):
-		R_a=np.sum(np_a[t:t+lookback-1])
+	T=n-lookback #ilyen hosszu lesz az output idosor
+	r_portfolio=np.empty([T]) #outputnak ures tomb, ami fel lesz toltve
+	for t in range(T): #vegig megyunk a napokon
+		R_a=np.sum(np_a[t:t+lookback-1]) #egyenlet (2)
 		R_b=np.sum(np_b[t:t+lookback-1])
 		sigma_a=np.std(np_a[t:t+lookback-1])
 		sigma_b=np.std(np_b[t:t+lookback-1])
 		S_a=abs((R_a/sigma_a)**pw)
 		S_b=abs((R_b/sigma_b)**pw)
-		w_a=math.copysign(S_a/(S_a+S_b), R_a)
+		w_a=math.copysign(S_a/(S_a+S_b), R_a) #egyenlet (3-4)
 		w_b=math.copysign(S_b/(S_a+S_b), R_b)
-		r_portfolio[t]=w_a*np_a[t+lookback]+w_b*np_b[t+lookback]
-		df_r = pd.DataFrame()
+		r_portfolio[t]=w_a*np_a[t+lookback]+w_b*np_b[t+lookback] #a portfolio hozama, egyenlet (5)
+		df_r = pd.DataFrame() #dataframe formara transzformalas
 		df_r['returns'] = r_portfolio
 	return df_r
 
-
-#df_returns=my_portfolio(r_gold,r_oil,20,2)
-#print(type(df_returns))
-#print(df_returns)
-
-c=np.percentile([-0.05,0.06,0.01],5)
-print(c)
-
-
+#VaR szamolo fuggveny:
 def calculate_historical_var(df_portfolio_returns, alpha):
 	np_ret=np.array(df_portfolio_returns)
 	pc=(1-alpha)*100
-	my_var=float(np.percentile(np_ret,pc))
+	my_var=float(np.percentile(np_ret,pc)) #VaR ertek adott percentilisbol
 	return my_var
 
+#Ellenorzes:
 #v=calculate_historical_var([-0.05,0.06,0.01],0.95)
 #print(type(v))
 #print(v)
 
+#parameter tombok megadasa
+#Ezektol fuggnek a portfolio sulyok
 par_lb=np.array([5,10,20,40,60,120])
 par_pw=np.array([1,2,3,4,5])
 
-table1=np.empty([par_lb.size,par_pw.size])
-
-for i in range(par_lb.size):
+table1=np.empty([par_lb.size,par_pw.size]) #ures tomb, a tablazatnak
+for i in range(par_lb.size): #vegig megyunk mindket parameter vektoron
 	for j in range(par_pw.size):
-		df_returns=my_portfolio(r_gold,r_oil,par_lb[i],par_pw[j])
-		table1[i,j]=calculate_historical_var(df_returns,0.95)*100
+		df_returns=my_portfolio(r_gold,r_oil,par_lb[i],par_pw[j]) #portfolio hozam generalas
+		table1[i,j]=calculate_historical_var(df_returns,0.95)*100 #VaR szamolas
 
 
 print(table1)
-
-df_export1 = pd.DataFrame(table1)
+df_export1 = pd.DataFrame(table1) #eredmeny exportalas
 df_export1.to_excel('py_results_1.xlsx')
 
 ###################################################################################################################
 
+#A hozam idosorokhoz tartozo allando atlag es szoras kiszamolasa
 exp_r_gold=np.mean(r_gold)
 exp_r_oil=np.mean(r_oil)
 vol_gold=np.std(r_gold)
 vol_oil=np.std(r_oil)
 
-#real correlation
+#valodi korrelacio a ket eszkoz kozott (0.11)
 combined = np.vstack((r_gold, r_oil))
 cr=np.corrcoef(combined)
 print(cr)
 
-
+#Korrelalt hozamokat szimulalo fuggveny
 def simulated_returns(expected_return, volatility, correlation, numOfSim):
 	T=numOfSim
 	expected_return2=exp_r_oil
 	volatility2=vol_oil
 	corr_M=np.array(([[1, correlation], [correlation, 1]]))
 	D_chol=np.linalg.cholesky(corr_M)
-	q=np.random.rand(T)
+	q=np.random.rand(T) #veletlen valtozok generalasa
 	rnd_z1=sc.stats.norm.ppf(q,loc=0,scale=1)
 	rnd_z2=sc.stats.norm.ppf(q,loc=0,scale=1)
-	corr_var1=rnd_z1*volatility+expected_return
-	corr_var2=(rnd_z1*D_chol[1,0]+rnd_z2*D_chol[1,1])*volatility2+expected_return2
+	corr_var1=rnd_z1*volatility+expected_return #1. hozam idosor
+	corr_var2=(rnd_z1*D_chol[1,0]+rnd_z2*D_chol[1,1])*volatility2+expected_return2 #2. hozam idosor, ami a parameter szerint korrelal
 	sim_arrs=np.vstack((corr_var1, corr_var2)).T
 	return sim_arrs
 
