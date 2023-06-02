e197151993faf821aba48581b2166bb72eeba57f
pleszboldi <70632121+pleszboldi@users.noreply.github.com>
2023-06-02 16:38:01 +0200

    Add files via upload
    1-4. feladat

--- submission.py
+++ submission.py
@@ -2,6 +2,8 @@ import pandas as pd
 import numpy as np
 import math
 import scipy as sc
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression 
 
 #Napi arfolyamok beolvasasa excelbol:
 P_daily = pd.read_excel('gold_oil_.xlsx')
@@ -54,9 +56,9 @@ for i in range(par_lb.size): #vegig megyunk mindket parameter vektoron
 		table1[i,j]=calculate_historical_var(df_returns,0.95)*100 #VaR szamolas
 
 
-print(table1)
-df_export1 = pd.DataFrame(table1) #eredmeny exportalas
-df_export1.to_excel('py_results_1.xlsx')
+#print(table1)
+#df_export1 = pd.DataFrame(table1) #eredmeny exportalas
+#df_export1.to_excel('py_results_1.xlsx')
 
 ###################################################################################################################
 
@@ -69,7 +71,7 @@ vol_oil=np.std(r_oil)
 #valodi korrelacio a ket eszkoz kozott (0.11)
 combined = np.vstack((r_gold, r_oil))
 cr=np.corrcoef(combined)
-print(cr)
+#print(cr)
 
 #Korrelalt hozamokat szimulalo fuggveny
 def simulated_returns(expected_return, volatility, correlation, numOfSim):
@@ -87,9 +89,6 @@ def simulated_returns(expected_return, volatility, correlation, numOfSim):
 	return sim_arrs
 
 arrs=simulated_returns(exp_r_gold, vol_gold, -0.01, 10)
-#print(arrs)
-#print(np.shape(arrs))
-#print(np.corrcoef(arrs.T))
 
 def my_portfolio_2(np_a,np_b,w_a,w_b):
 	T=np_a.size
@@ -110,8 +109,12 @@ for i in range(corrs.size):
 	portfolio_2=my_portfolio_2(asset1,asset2,w_gold,w_oil)
 	abra1[i]=calculate_historical_var(portfolio_2,0.95)*100
 
-
-print(abra1)
+mycolor=(150/255,5/255,160/255)
+figure,axes=plt.subplots()
+axes.plot(corrs,table2,color=mycolor)
+axes.set(xlabel='Korreláció',ylabel='VaR értékek',title='Diverzifikáció korrelált hozamokkal')
+#figure.savefig('abra1.png')
+#plt.show()
 
 ###################################################################################################################
 
@@ -133,7 +136,123 @@ def calculate_ewma_variance(df_etf_returns, decay_factor, window):
 	return df_ewma
 
 ew=calculate_ewma_variance(df_r_etf, 0.97, 100)
-print(type(ew))
-print(ew)
+figure,axes=plt.subplots()
+axes.plot(range(ew.size),ew*100,color=mycolor)
+axes.set(xlabel='Idő',ylabel='EWMA variancia',title='EWMA variancia alakulása %-ban')
+#figure.savefig('abra2.png')
+#plt.show()
 
+###################################################################################################################
+
+P_etf_daily = pd.read_excel('msci_etf.xlsx')
+np_P_etf=np.array(P_etf_daily)
+r_etf=np.log(np.divide(np.subtract(np_P_etf[1:,0], np_P_etf[:-1,0]),np_P_etf[:-1,0])+1)
+r2_etf=r_etf**2
+
+S1=r2_etf[:400]
+S2=r2_etf[401:801]
+S3=r2_etf[802:]
+
+def my_ar(ts,lags):
+	T=ts.size-lags
+	y=ts[lags:]
+	x=np.empty([T,lags])
+	for i in range(lags):
+		x[:,i]=ts[i:T+i]
+	model = LinearRegression().fit(x, y)
+	y_pred = model.predict(x)
+	residuals=y-y_pred
+	return model, residuals
+
+
+def my_model_param(ts, lags):
+	m_ar_1 = my_ar(ts,lags)
+	model1=m_ar_1[0]
+	#coeffs1=model1.coef_
+	res1=m_ar_1[1]
+	m_ar_2 = my_ar(res1,lags)
+	model2=m_ar_2[0]
+	#coeffs2=model2.coef_
+	#cfs=np.vstack((coeffs1, coeffs2)).T
+	return model1, model2
+
+#cfs=my_model_param(S1, 3)
+#print(cfs)
+
+def calculate_ewma_variance_1(df_etf_returns, decay_factor, window,b):
+	init_p=b
+	etf_ret=np.array(df_etf_returns)
+	etf_ret1=etf_ret[:init_p]
+	etf_ret2=etf_ret[init_p:]
+	ewma=np.empty([window+1])
+	ewma[0]=np.var(etf_ret1)
+	for i in range(window):
+		ewma[i+1]=(1-decay_factor)*ewma[i]+decay_factor*etf_ret2[i+1]**2
+	return ewma
+
+def my_model_pred(S2, max_lag,y,T):
+	#T=S2.size-max_lag
+	my_mse=np.empty(max_lag)
+	for i in range(1,max_lag+1,1):
+		md_i=my_model_param(S2, i)
+		md_r=md_i[0]
+		ar_int=md_r.intercept_
+		ar_cfs=md_r.coef_
+		x=np.empty([T,i])
+		for j in range(i):
+			x[:,j-1]=S2[max_lag-j:S2.size-j]
+			#print(np.shape(x))
+		r_ar_pred=np.ones((T,1))*ar_int
+		#print(np.shape(r_ar_pred))
+		for k in range(i):
+			for l in range(T-1):
+				r_ar_pred[l,0]=r_ar_pred[l,0]+ar_cfs[k]*x[l,k]
+			#r_ar_pred=np.add(r_ar_pred,ar_cfs[k]*x[:,k])
+		my_mse[i-1]=np.sum(np.square(np.subtract(y,r_ar_pred)))/(T-1)
+	residuals=y-r_ar_pred
+	return residuals, my_mse, r_ar_pred
+
+max_lag=20
+T=S2.size-max_lag
+y1=S2[max_lag:]
+md_p1=my_model_pred(S2, max_lag,y1,T)
+res1=md_p1[0]
+y2=calculate_ewma_variance_1(S2, 0.97, T-1,max_lag)
+
+md_p2=my_model_pred(S2, max_lag,y2,T)
+
+var_mse=md_p2[1]
+#print(var_mse[6]*100)
+
+mycolor=(150/255,5/255,160/255)
+figure,axes=plt.subplots()
+axes.plot(range(1,max_lag+1,1),var_mse,color=mycolor)
+axes.set(xlabel='Késleltetés',ylabel='MSE',title='Variancia előrejelzési hiba')
+#figure.savefig('abra4.png')
+#plt.show()
+
+def my_model_pred_2(S2, lag,y,T):
+	#T=S2.size-max_lag
+	i=lag
+	max_lag=lag
+	md_i=my_model_param(S2, i)
+	md_r=md_i[0]
+	ar_int=md_r.intercept_
+	ar_cfs=md_r.coef_
+	x=np.empty([T,i])
+	for j in range(i):
+		x[:,j-1]=S2[max_lag-j:S2.size-j]
+	r_ar_pred=np.ones((T,1))*ar_int
+	for k in range(i):
+		for l in range(T-1):
+			r_ar_pred[l,0]=r_ar_pred[l,0]+ar_cfs[k]*x[l,k]
+		#r_ar_pred=np.add(r_ar_pred,ar_cfs[k]*x[:,k])
+	my_mse=np.sum(np.square(np.subtract(y,r_ar_pred)))/(T-1)
+	return  my_mse
+
+
+T3=S3.size-7
+y3=calculate_ewma_variance(S3, 0.97, T3-1,7)
+md_p3=my_model_pred_2(S3, 7,y3,T3)
+#print(md_p3*100)
 
